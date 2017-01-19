e8178ab5b5e4e7a8b98f8b5fab0e7fa5ceeeda2f
Rémi Eisack <Rmi Eisack>
2017-01-19 22:16:41 +0100

    Utilisation de bluebird comme systeme de promise a la place de mpromise

--- server/config/config.js
+++ server/config/config.js
@@ -3,17 +3,26 @@
 
     var path = require('path');
     var rootPath = path.normalize(__dirname + '/../../');
+    var bluebird = require('bluebird');
+
 //noinspection JSUnresolvedVariable
     module.exports = {
         developpement: {
             db: 'mongodb://localhost/multivisionplus',
             rootPath: rootPath,
-            port: process.env.PORT || 3030
+            port: process.env.PORT || 3030,
+            options: {
+                promiseLibrary: bluebird
+            }
+
         },
         production: {
             db: process.env.MONGODB_URI,
             rootPath: rootPath,
-            port: process.env.PORT || 80
+            port: process.env.PORT || 80,
+            options: {
+                promiseLibrary: bluebird
+            }
         }
     };
 
--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -4,9 +4,11 @@
     var mongoose = require('mongoose');
     var userModel = require('../models/User');
     var courseModel = require('../models/Course');
+    var bluebird = require('bluebird');
 
     module.exports = function (config) {
-        mongoose.connect(config.db);
+        mongoose.connect(config.db, config.options);
+        mongoose.Promise = bluebird;
 
         var db = mongoose.connection;
         db.on('error', console.error.bind(console, 'connection error...'));
